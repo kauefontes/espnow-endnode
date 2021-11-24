de7242bdef6b2a72d30a0c78a656ef4d98687338
Kaue Pereira <kauefontes@outlook.com>
2021-11-23 22:16:47 -0400

    Added setup and loop methods and required libs

---
+++ endnode.ino
@@ -0,0 +1,14 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+#define CHANNEL = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
