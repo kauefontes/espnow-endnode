84e7547361ba96c759d072702c8b42adf8a537bc
Kaue Pereira <kauefontes@outlook.com>
2021-11-23 23:17:17 -0400

    feat: initial setup method

--- endnode.ino
+++ endnode.ino
@@ -1,24 +1,32 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-#define CHANNEL = 2;
+#define CHANNEL 2
 
-void setup() {
-  // put your setup code here, to run once:
-
+void setup()
+{
+  initialSetup();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void initialSetup()
+{
+  Serial.begin(115200);
+  Serial.println("Starting endnode...");
+  WiFi.mode(WIFI_AP);
 }
 
-void initESPNow() {
+void startEspnow()
+{
   WiFi.disconnect();
-  if (esp_now_init() == ESP_OK) {
+  if (esp_now_init() == ESP_OK)
+  {
     Serial.println("ESPNow Init Success");
-  } else {
-    Serial.println("ESPNowinit failed");
+  }
+  else
+  {
+    Serial.println("ESPNowinit failed, rebooting now...");
     ESP.restart();
   }
 }
+
+void loop() {}
