4a7e0fcd88f6d480586e9c287a9065d0d0e03afa
Kaue Pereira <kauefontes@outlook.com>
2021-11-23 23:20:01 -0400

    feat: added espNow callback method

--- endnode.ino
+++ endnode.ino
@@ -7,6 +7,7 @@ void setup()
 {
   initialSetup();
   setupAP();
+  startEspnow();
 }
 
 void initialSetup()
@@ -45,6 +46,19 @@ void startEspnow()
     Serial.println("ESPNowinit failed, rebooting now...");
     ESP.restart();
   }
+  esp_now_register_recv_cb(onDataRecv);
+}
+
+void onDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len)
+{
+  char macAdrStr[18];
+  snprintf(macAdrStr, sizeof(macAdrStr), "%02x:%02x:%02x:%02x:%02x:%02x", mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  handleCommand();
+  Serial.print("Last Packet Received from: ");
+  Serial.println(macAdrStr);
+  Serial.print("Last Packet Received Data: ");
+  Serial.println(*data);
+  Serial.println("");
 }
 
 void loop() {}
